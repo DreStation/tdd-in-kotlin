3a2495fb8422854f17b7b0a549c6c486f1ab6a14
DreStation <drestation1@gmail.com>
2023-04-28 21:24:08 -0400

    Further testing and code optimizations

--- src/main/kotlin/DesertEagle.kt
+++ src/main/kotlin/DesertEagle.kt
@@ -1,12 +1,29 @@
 class DesertEagle {
-    private var magazine = 7
+    var magazine = 7
+    var isSafetyActive = true
 
     fun shoot(triggerPulls: Int): Int {
-        // If firing more than 7 times
-        if (magazine < triggerPulls || triggerPulls < 1)
+        // Don't shoot unless safety is deactivated
+        if (isSafetyActive) throw IllegalArgumentException("Cannot shoot when safety is active")
+
+        magazine = when (triggerPulls) {
+            in Int.MIN_VALUE..0 -> 7 // Can't shoot 0 or negative times, do nothing
+            in 1..6 -> --magazine // Normal shooting 💥🔫
+            else -> 0 // Mag emptied from shooting more than 6 times
+        }
+        return magazine
+
+        /*
+        // If firing more than 7 times, empty mag
+        if (triggerPulls > magazine)
             magazine = 0
+
+        // If 0 or negative input, do nothing
+        else if (triggerPulls < 1)
+            return magazine
+
         else
             magazine -= triggerPulls
-        return magazine
+        return magazine*/
     }
 }
--- src/test/kotlin/DesertEagleTest.kt
+++ src/test/kotlin/DesertEagleTest.kt
@@ -1,32 +1,54 @@
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions.assertAll
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.api.function.Executable
 import kotlin.test.assertEquals
 
 class DesertEagleTest {
     @Test
+    @DisplayName("Ensure you can't shoot when safety is active")
+    fun testShootWhenSafetyIsActive() {
+        val de = DesertEagle()
+        val exception = assertThrows<IllegalArgumentException> { de.shoot(1) }
+        assertEquals("Cannot shoot when safety is active", exception.message)
+    }
+
+    @Test
+    @DisplayName("Shoot once and have 6 bullets remaining")
     fun testShootOnce() {
         val de = DesertEagle()
+        de.isSafetyActive = false
         val result = de.shoot(1)
         assertEquals(6, result)
     }
 
     @Test
+    @DisplayName("Shoot 7 times and have 0 bullets remaining")
     fun testShootUntilEmpty() {
         val de = DesertEagle()
+        de.isSafetyActive = false
         val result = de.shoot(7)
         assertEquals(0, result)
     }
 
     @Test
+    @DisplayName("Shoot more than 7 times and ensure magazine isn't negative")
     fun testShootWhileEmpty() {
         val de = DesertEagle()
+        de.isSafetyActive = false
         val result = de.shoot(14)
         assertEquals(0, result)
     }
 
     @Test
-    fun testShootNegativeInt() {
+    @DisplayName("Ensure nothing happens when inputting 0 or negative ints")
+    fun testShootWithNegativeInput() {
         val de = DesertEagle()
-        val result = de.shoot(-1)
-        assertEquals(0, result)
+        de.isSafetyActive = false
+        assertAll(
+            Executable { assertEquals(7, de.shoot(0)) },
+            Executable { assertEquals(7, de.shoot(-1)) }
+        )
     }
 }
